4a55a7e0b658b4904688931fa8deccba9dcfeab1
Mike Lock <michael.lockwitz@consensys.net>
2019-03-15 11:21:03 -0600

    Cleaning up modal styles and adding close function.

--- src/components/WrongNetworkModal.js
+++ src/components/WrongNetworkModal.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { Card, Heading, Text, Flex, ToastMessage, Icon, Modal } from "rimble-ui";
+import { Card, Heading, Text, Flex, ToastMessage, Icon, Modal, TextButton } from "rimble-ui";
 
 class WrongNetworkModal extends React.Component {
   render() {
     return (
       <Modal isOpen={this.props.isOpen}>
-        <Card p={4}>
+        <Card p={5} maxWidth={'600px'}>
+          <TextButton
+            icononly
+            icon={'Close'}
+            color={'moon-gray'}
+            position={'absolute'}
+            top={0}
+            right={0}
+            mt={3}
+            mr={3}
+            onClick={this.props.closeWrongNetworkModal}
+          />
+
           <Flex flexDirection={"column"} justifyContent={"space-between"}>
-            <Flex justifyContent={"center"}>
+            <Flex justifyContent={"center"} my={4}>
               <Icon name="Warning" color="gold" size="40"/>
             </Flex>
             
-            <Heading.h2 mt={3}>Wrong Network</Heading.h2>
-            <Text mb={3}>Looks like your account is connected to {this.props.currentNetwork.name} network. Head to MetaMask and switch to the {this.props.requiredNetwork.name} network.</Text>
+            <Heading.h2 my={3}>Wrong Network</Heading.h2>
+
+            <Text my={4}>Looks like your account is connected to {this.props.currentNetwork.name} network. Head to MetaMask and switch to the {this.props.requiredNetwork.name} network.</Text>
             
             <ToastMessage
               message={'Waiting for the right network.'}
