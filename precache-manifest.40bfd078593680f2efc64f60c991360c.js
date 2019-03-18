6db6bd21b2bd4e543e54d9cc500503d720e0ed2a
Mike Lock <michael.lockwitz@consensys.net>
2019-03-18 15:13:34 -0600

    Updates

---
+++ precache-manifest.40bfd078593680f2efc64f60c991360c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "040bab33c41aa0c6baa8",
+    "url": "/rimble-app-demo/static/js/runtime~main.040bab33.js"
+  },
+  {
+    "revision": "66f2ad7293d26edef561",
+    "url": "/rimble-app-demo/static/js/main.66f2ad72.chunk.js"
+  },
+  {
+    "revision": "f9886c6336a26b0b4b48",
+    "url": "/rimble-app-demo/static/js/1.f9886c63.chunk.js"
+  },
+  {
+    "revision": "4596f2bb0e80436df4521ed07152517f",
+    "url": "/rimble-app-demo/index.html"
+  }
+];
