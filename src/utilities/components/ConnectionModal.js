9082fa2bbf17c2790919de0afbe4cb549b63ad82
Mike Lock <michael.lockwitz@consensys.net>
2019-05-15 16:44:55 -0600

    Added about tx fees to connection modal.

--- src/utilities/components/ConnectionModal.js
+++ src/utilities/components/ConnectionModal.js
@@ -13,6 +13,19 @@ import {
 } from "rimble-ui";
 
 class ConnectionModal extends React.Component {
+  state = {
+    showTxFees: false
+  };
+
+  toggleShowTxFees = e => {
+    console.log("showTxFees", this.state.showTxFees);
+    e.preventDefault();
+
+    this.setState({
+      showTxFees: !this.state.showTxFees
+    });
+  };
+
   render() {
     return (
       <Modal isOpen={this.props.isOpen}>
@@ -26,59 +39,85 @@ class ConnectionModal extends React.Component {
             />
           </Flex>
 
-          <Box mb={3}>
-            <Heading.h2>Before you connect</Heading.h2>
-            <Text my={3}>
-              Connecting lets you use the Rimble Demo App via your Ethereum
-              account.
-            </Text>
-          </Box>
+          {this.state.showTxFees === false ? (
+            <Box>
+              <Box mb={3}>
+                <Heading.h2>Before you connect</Heading.h2>
+                <Text my={3}>
+                  Connecting lets you use the Rimble Demo App via your Ethereum
+                  account.
+                </Text>
+              </Box>
 
-          <Flex
-            my={4}
-            flexWrap={"wrap"}
-            justifyContent={"space-between"}
-            mx={-2}
-          >
-            <Box width={[1, 1, 1 / 3]} px={2}>
-              <Flex justifyContent={"center"}>
-                <Icon color="primary" size="60" name="Public" />
+              <Flex
+                my={4}
+                flexWrap={"wrap"}
+                justifyContent={"space-between"}
+                mx={-2}
+              >
+                <Box width={[1, 1, 1 / 3]} px={2}>
+                  <Flex justifyContent={"center"}>
+                    <Icon color="primary" size="60" name="Public" />
+                  </Flex>
+                  <Heading.h4>The blockchain is public</Heading.h4>
+                  <Text>
+                    Your Ethereum account activity is public on the blockchain.
+                    Choose an account you don’t mind being linked with your
+                    activity here.
+                  </Text>
+                </Box>
+                <Box width={[1, 1, 1 / 3]} px={2}>
+                  <Flex justifyContent={"center"}>
+                    <Icon
+                      color="primary"
+                      size="60"
+                      name="AccountBalanceWallet"
+                    />
+                  </Flex>
+                  <Heading.h4>Have some Ether for fees</Heading.h4>
+                  <Text>
+                    You’ll need Ether to pay transaction fees. Buy Ether from
+                    exchanges like Coinbase.
+                  </Text>
+                  <Link
+                    title="Learn about Ethereum transaction fees"
+                    as={"a"}
+                    href="#"
+                    onClick={this.toggleShowTxFees}
+                  >
+                    What are transaction fees?
+                  </Link>
+                </Box>
+                <Box width={[1, 1, 1 / 3]} px={2}>
+                  <Flex justifyContent={"center"}>
+                    <Icon color="primary" size="60" name="People" />
+                  </Flex>
+                  <Heading.h4>Have the right account ready</Heading.h4>
+                  <Text>
+                    If you have multiple Ethereum accounts, check that the one
+                    you want to use is active in your browser.
+                  </Text>
+                </Box>
               </Flex>
-              <Heading.h4>The blockchain is public</Heading.h4>
-              <Text>
-                Your Ethereum account activity is public on the blockchain.
-                Choose an account you don’t mind being linked with your activity
-                here.
-              </Text>
+              <Flex borderTop={1} borderColor={"#999"} />
+              <Box mt={3} textAlign="right">
+                <MetaMaskButton onClick={this.props.validateAccount}>
+                  Connect with MetaMask
+                </MetaMaskButton>
+              </Box>
             </Box>
-            <Box width={[1, 1, 1 / 3]} px={2}>
-              <Flex justifyContent={"center"}>
-                <Icon color="primary" size="60" name="AccountBalanceWallet" />
-              </Flex>
-              <Heading.h4>Have some Ether for fees</Heading.h4>
-              <Text>
-                You’ll need Ether to pay transaction fees. Buy Ether from
-                exchanges like Coinbase.
-              </Text>
-              <Link>What are transaction fees?</Link>
+          ) : (
+            <Box mb={3}>
+              <Heading.h2>Transaction Fees</Heading.h2>
+              <Text my={3}>About tx fees.</Text>
+              <Flex borderTop={1} borderColor={"#999"} />
+              <Box mt={3} textAlign="right">
+                <MetaMaskButton onClick={this.props.validateAccount}>
+                  Connect with MetaMask
+                </MetaMaskButton>
+              </Box>
             </Box>
-            <Box width={[1, 1, 1 / 3]} px={2}>
-              <Flex justifyContent={"center"}>
-                <Icon color="primary" size="60" name="People" />
-              </Flex>
-              <Heading.h4>Have the right account ready</Heading.h4>
-              <Text>
-                If you have multiple Ethereum accounts, check that the one you
-                want to use is active in your browser.
-              </Text>
-            </Box>
-          </Flex>
-          <Flex borderTop={1} borderColor={"#999"} />
-          <Box mt={3} textAlign="right">
-            <MetaMaskButton onClick={this.props.validateAccount}>
-              Connect with MetaMask
-            </MetaMaskButton>
-          </Box>
+          )}
         </Card>
       </Modal>
     );
