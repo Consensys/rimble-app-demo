0414cd9b2adf84f2fa007bde2186fe5388811640
Ryan Cordell <ryan.cordell@consensys.net>
2019-05-16 08:14:24 +0100

    What are transaction fees copy updated

--- src/utilities/components/ConnectionModal.js
+++ src/utilities/components/ConnectionModal.js
@@ -108,13 +108,53 @@ class ConnectionModal extends React.Component {
             </Box>
           ) : (
             <Box mb={3}>
-              <Heading.h2>Transaction Fees</Heading.h2>
-              <Text my={3}>About tx fees.</Text>
+              <Heading.h2>Transaction fees</Heading.h2>
+              <Text my={3}>You need to pay a fee to use the Ethereum blockchain. This pays for someone to process your transaction and store the data.</Text>
+              <Heading.h4>What are you paying for?</Heading.h4>
+              <Flex
+                my={4}
+                flexWrap={"wrap"}
+                justifyContent={"space-between"}
+                mx={-2}
+              >
+                <Box width={[1, 1, 1 / 3]} px={2}>
+                  <Flex justifyContent={"center"}>
+                    <Icon color="primary" size="60" name="Fingerprint" />
+                  </Flex>
+                  <Heading.h4>Undeniable proof</Heading.h4>
+                  <Text>
+                    You get a public record of any funds you send or receive, a bit like a deed for a house.
+                  </Text>
+                </Box>
+                <Box width={[1, 1, 1 / 3]} px={2}>
+                  <Flex justifyContent={"center"}>
+                    <Icon
+                      color="primary"
+                      size="60"
+                      name="EnhancedEncryption"
+                    />
+                  </Flex>
+                  <Heading.h4>Unbreakable encryption</Heading.h4>
+                  <Text>
+                    Your funds can only ever go to your intended recipients.
+                  </Text>
+                </Box>
+                <Box width={[1, 1, 1 / 3]} px={2}>
+                  <Flex justifyContent={"center"}>
+                    <Icon color="primary" size="60" name="AccountBalance" />
+                    <Icon color="primary" size="60" name="NotInterested" />
+                  </Flex>
+                  <Heading.h4>Unparalleled control</Heading.h4>
+                  <Text>
+                    You can pay or get paid without using any banks or companies.
+                  </Text>
+                </Box>
+              </Flex>
               <Flex borderTop={1} borderColor={"#999"} />
               <Box mt={3} textAlign="right">
-                <MetaMaskButton onClick={this.props.validateAccount}>
-                  Connect with MetaMask
-                </MetaMaskButton>
+                <Button.Outline onClick={this.toggleShowTxFees}>
+                  Go back
+                </Button.Outline>
               </Box>
             </Box>
           )}
